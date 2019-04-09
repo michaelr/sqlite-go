75d4597d97d456eac29f5e923b4769ceb0fc655e
Michael Reddick <michael.reddick@gmail.com>
2019-04-08 21:55:33 -0500

    Add comments

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// sqlite-go is a go implementation of sqlite
 package main
 
 import (
@@ -9,12 +10,15 @@ import (
 	"strings"
 )
 
+// rawSQL is a string of SQL
 type rawSQL string
 
+// RawSQL returns a string represention of the rawSQL
 func (s rawSQL) RawSQL() string {
 	return string(s)
 }
 
+// sqlStatement is sql statement that's been parsed and is ready to execute.
 type sqlStatement interface {
 	RawSQL() string
 }
@@ -26,6 +30,8 @@ type selectStatement struct {
 }
 
 func main() {
+
+	// This is the REPL loop
 	for {
 		fmt.Print("db > ")
 		input := readInput()
@@ -49,6 +55,7 @@ func main() {
 	}
 }
 
+// readInput reads from Stdin and trims the surrounding whitespace.
 func readInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, err := reader.ReadString('\n')
@@ -59,6 +66,8 @@ func readInput() string {
 	return strings.TrimSpace(text)
 }
 
+// doMetaCmd executes sqlite commands that start with a "."
+// Meta commands are non-sql. For example, ".exit"
 func doMetaCmd(cmd string) error {
 	switch cmd {
 	case ".exit":
@@ -69,6 +78,8 @@ func doMetaCmd(cmd string) error {
 	return nil
 }
 
+// prepareStatement parses a SQL string and returns the corresponding
+// sqlStatement according to what sql statement it is. insert, select, etc.
 func prepareStatement(input string) (sqlStatement, error) {
 	switch {
 	case strings.HasPrefix(input, "insert"):
@@ -80,6 +91,7 @@ func prepareStatement(input string) (sqlStatement, error) {
 	}
 }
 
+// executeStatement runs a sqlStatement
 func executeStatement(statement sqlStatement) error {
 	switch statement.(type) {
 	case *insertStatement:
